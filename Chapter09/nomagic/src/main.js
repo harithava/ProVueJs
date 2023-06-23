6a5969ec5235a3953c009582c0daec03594b9919
harithava <134498340+harithava@users.noreply.github.com>
2023-06-24 00:23:24 +0100

    HH09-8: Adding Data to the Vue Object.

--- Chapter09/nomagic/src/main.js
+++ Chapter09/nomagic/src/main.js
@@ -7,11 +7,14 @@ new Vue({
     template:
         `<div class="text-center p-3">
             <h1 class="bg-secondary text-white p-3">
-                Vue: Button Not Pressed
+                Button Presses: {{ counter }}
             </h1>
             <button class="btn btn-secondary">
                 Press Me
             </button>
-        </div>`
+        </div>`,
+    data: {
+        counter: 0
+    }
 });
 
