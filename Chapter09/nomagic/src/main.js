26e43421a8209eb928ffa8d6e846155bc970b6c3
harithava <134498340+harithava@users.noreply.github.com>
2023-06-24 01:42:07 +0100

    HH09-13: Registering and Applying Components.

--- Chapter09/nomagic/src/main.js
+++ Chapter09/nomagic/src/main.js
@@ -1,30 +1,10 @@
 require('../node_modules/bootstrap/dist/css/bootstrap.min.css')
 
 import Vue from "vue"
+import MyComponent from "./App";
 
 new Vue({
     el: "#app",
-    template:
-        `<div class="text-center p-3">
-            <h1 class="bg-secondary text-white p-3">
-                {{ message }}
-            </h1>
-            <button class="btn btn-secondary" v-on:click="handleClick">
-                Press Me
-            </button>
-        </div>`,
-    data: {
-        counter: 0
-    },
-    methods: {
-        handleClick() {
-            this.counter++;
-        }
-    },
-    computed: {
-        message() {
-            return this.counter == 0 ? "Button Not Pressed" : `Button Presses: ${this.counter}`;
-        }
-    }
+    components: { "custom": MyComponent },
+    template: `<custom/>`
 });
-
