ac85f4a02e8198e37a15ff05e3563dd1f44f0aeb
harithava <134498340+harithava@users.noreply.github.com>
2023-06-28 02:19:53 +0100

    HH12-4:Sanitising the data values.

--- Chapter12/templatesanddata/src/App.vue
+++ Chapter12/templatesanddata/src/App.vue
@@ -2,6 +2,7 @@
     <div class="container-fluid text-center">
         <div class="bg-primary text-white m-2 p-3">
             <h3>Product: <span v-text="name"></span></h3>
+            <span v-text="fragment"></span>
         </div>
         <button v-on:click="handleClick" class="btn btn-primary">
             Press Me
@@ -14,7 +15,10 @@ export default {
   name: 'MyComponent',
   data: function () {
     return {
-        name: "Lifejacket"
+        name: "Lifejacket",
+        fragment: `<div class="form-group">
+                     Password <input class="form-control"/>
+                    </div>`
     }
   },
   methods: {
