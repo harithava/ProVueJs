a78590132301b920238d1f127c88846f6feda2d0
harithava <134498340+harithava@users.noreply.github.com>
2023-06-28 11:55:03 +0100

    HH12-10: Simplifying Content Selection.

--- Chapter12/templatesanddata/src/App.vue
+++ Chapter12/templatesanddata/src/App.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid text-center">
         <div class="bg-primary text-white m-2 p-3">
             <h3 v-if="showElements">Product: {{ name }}</h3>
-            <h3 v-if="!showElements">Product: {{ price }}</h3>
+            <h3 v-else>Product: {{ price }}</h3>
         </div>
         <button v-on:click="handleClick" class="btn btn-primary">
             Press Me
