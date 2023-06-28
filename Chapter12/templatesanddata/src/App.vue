d492d2cdcfa08157d7d930091e1b8c0c9050f991
harithava <134498340+harithava@users.noreply.github.com>
2023-06-28 11:52:09 +0100

    HH12-9: Choosing Content to Display.

--- Chapter12/templatesanddata/src/App.vue
+++ Chapter12/templatesanddata/src/App.vue
@@ -1,15 +1,8 @@
 <template>
     <div class="container-fluid text-center">
         <div class="bg-primary text-white m-2 p-3">
-            <h3>Product: <span v-text="name"></span></h3>
-            <ul class="text-left">
-                <li>List item</li>
-                <template v-if="showElements">
-                    <li>{{name}}</li>
-                    <li>{{price}}</li>
-                </template>
-                <li>Other list item</li>
-            </ul>
+            <h3 v-if="showElements">Product: {{ name }}</h3>
+            <h3 v-if="!showElements">Product: {{ price }}</h3>
         </div>
         <button v-on:click="handleClick" class="btn btn-primary">
             Press Me
